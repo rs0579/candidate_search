3f2dccc51910655c4b0d8556e10ce4e9a9353106
rs0579 <raheemsenegal@outlook.com>
2025-02-23 18:40:06 +0100

    modified logic in CandidateCard component

--- src/components/CandidateCard.tsx
+++ src/components/CandidateCard.tsx
@@ -1,17 +1,11 @@
-import react from 'react';
 import Candidate from '../interfaces/Candidate.interface';
-import { BsPlusCircleFill } from "react-icons/bs";
-import { HiMinusCircle } from "react-icons/hi";
-import { searchGithubUser, searchGithub } from '../api/API';
+
 
 type CandidateCardProps = {
     currentCandidate: Candidate;
-    searchGithubUser?: (() => void) | null;
-    searchGithub?: ((
-        e: react.MouseEvent<HTMLButtonElement, MouseEvent>,    ) => void) | null;
 }
 
-const CandidateCard = ({ currentCandidate, searchGithubUser, searchGithub }: CandidateCardProps) => {
+const CandidateCard = ({ currentCandidate }: CandidateCardProps) => {
     return (
         <div className='card'>
             <img src={currentCandidate.avatar_url} alt={`${currentCandidate.username}`} />

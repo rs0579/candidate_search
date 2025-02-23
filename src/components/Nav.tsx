53b42d288d3e806052f1b9359a18e4d997396a68
rs0579 <raheemsenegal@outlook.com>
2025-02-23 03:05:02 +0100

    added an interface. added a Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,21 @@
+import { Link, useLocation } from 'react-router-dom';
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
+  const currentPage = useLocation().pathname;
   return (
-    <div>Nav</div>
+    <div id='nav'>
+      <h3>
+        <Link to="/">
+          Home
+        </Link>
+      </h3>
+      <h3>
+        <Link
+          to="/potentialcandidates">
+          Potential Candidates
+        </Link>
+      </h3>
+    </div>
   )
 };
 
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,9 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    name?: string;
+    username: string;
+    location?: string;
+    html_url: string;
+    company?: string;
+    avatar_url: string;
+}
